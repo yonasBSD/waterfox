29ef9f62fba57ccea828f20ce2c2d753321751ea
Louie Torres <30176969+louie-github@users.noreply.github.com>
2023-01-12 00:25:34 +0800

    feat: extended jxl support

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -1288,6 +1288,7 @@ function maybeRecordToHandleTelemetry(uri, isLaunch) {
       ".xhtml",
       ".svg",
       ".webp",
+      ".jxl",
     ]);
     if (registeredExtensions.has(extension)) {
       counter[extension].add(1);
