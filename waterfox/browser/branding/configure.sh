5687efe8cb87ade015dd2e6da08902190484267a
Alex Kontos <MrAlex94@users.noreply.github.com>
2025-08-03 18:35:42 +0200

    feat: add waterfox branding

---
+++ waterfox/browser/branding/configure.sh
@@ -0,0 +1,14 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+MOZ_APP_DISPLAYNAME=Waterfox
+MOZ_APP_NAME=waterfox
+MOZ_DISTRIBUTION_ID=net.waterfox
+MOZ_INCLUDE_SOURCE_INFO=1
+MOZ_REQUIRE_SIGNING=
+MOZ_TELEMETRY_REPORTING=
+MOZ_ASYNCIHANDLERCONTROL_IID="fb2703de-e41b-4565-a50b-777f44879c65"
+MOZ_HANDLER_CLSID="af069df1-ed1c-4994-9097-8c033d09539c"
+MOZ_IGECKOBACKCHANNEL_IID="6f2c203c-73b4-4f74-8a79-1e99202815e3"
+MOZ_IHANDLERCONTROL_IID="713883ba-0f4e-481e-823e-e1cb9a7a283c"
